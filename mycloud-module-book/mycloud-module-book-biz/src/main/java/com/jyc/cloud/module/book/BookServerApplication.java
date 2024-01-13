51c57334dd72d30452b5620e6ede7a063bffee66
JiaYongChao <jiayongchao258@163.com>
2024-01-13 18:22:55 +0800

    修改项目配置，新增book模块

---
+++ mycloud-module-book/mycloud-module-book-biz/src/main/java/com/jyc/cloud/module/book/BookServerApplication.java
@@ -0,0 +1,18 @@
+package com.jyc.cloud.module.book;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+/**
+ * @ClassNAME BookServerApplication
+ * @Description 项目启动类
+ * @Author jiayongchao
+ * @Date 2024/1/13 17:51
+ * @Version 1.0
+ */
+@SpringBootApplication
+public class BookServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookServerApplication.class, args);
+    }
+}
---
+++ mycloud-module-book/mycloud-module-book-biz/src/main/java/com/jyc/cloud/module/book/controller/app/AppBookTestController.java
@@ -0,0 +1,31 @@
+package com.jyc.cloud.module.book.controller.app;
+
+import com.jyc.cloud.framework.common.pojo.CommonResult;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static com.jyc.cloud.framework.common.pojo.CommonResult.success;
+
+/**
+ * @ClassNAME AppBookTestController
+ * @Description 测试
+ * @Author jiayongchao
+ * @Date 2024/1/13 18:12
+ * @Version 1.0
+ */
+@Tag(name = "用户 App - Test")
+@RestController
+@RequestMapping("/book/test")
+@Validated
+public class AppBookTestController {
+
+    @GetMapping("/get")
+    @Operation(summary = "获取 test 信息")
+    public CommonResult<String> get() {
+        return success("true");
+    }
+}
